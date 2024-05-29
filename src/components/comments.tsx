04348f790833bf697c37c95f73b2c8db0af1eb68
Toby2210 <106552704+Toby2210@users.noreply.github.com>
2024-05-29 05:29:38 +0000

    Addind a comment button when adding a comment

--- src/components/comments.tsx
+++ src/components/comments.tsx
@@ -1,135 +1,133 @@
-
-import { Button,  List, Tooltip, Space, Input, Modal, Flex } from  'antd';
+import { Button, List, Tooltip, Space, Input, Modal, Flex, Row, Col } from 'antd';
 import { Comment } from '@ant-design/compatible';
-import { DeleteOutlined, DeleteFilled, MessageOutlined, } from '@ant-design/icons';
-import React, { useState } from 'react';
+import { DeleteOutlined, DeleteFilled, MessageOutlined } from '@ant-design/icons';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { getCurrentUser } from "../services/auth.service";
+import { getCurrentUser } from '../services/auth.service';
 import { NavigateFunction, useNavigate } from 'react-router-dom';
 import { api } from './common/http-common';
 
-const DisplayComment = (props:any) => {    
-const [article_comments, setComments] = React.useState<any>([]);
-const [isShow, setIsShow] = React.useState(false); 
-const currentUser = getCurrentUser();
-const navigate: NavigateFunction = useNavigate();
+const DisplayComment = (props: any) => {
+  const [article_comments, setComments] = useState<any>([]);
+  const [isShow, setIsShow] = useState(false);
+  const currentUser = getCurrentUser();
+  const navigate: NavigateFunction = useNavigate();
 
-let islogin =false;
-let isAdmin =false;
-let Icon = DeleteOutlined
+  let islogin = false;
+  let isAdmin = false;
+  let Icon = DeleteOutlined;
 
-if(currentUser){
-  islogin =true
-  if(currentUser.role=="admin" )
-  isAdmin=true}
- 
-  React.useEffect(()=>{
-    const msgLink = `${api.uri}/articles/${props.id}/msg`
-  axios.get(msgLink)
-    .then((res)=>{
-      setComments(res.data);  
-      console.log('no of msg ',article_comments.length)                    
-    }) 
-    .catch(err => {
-      console.log(`icon error for msg  `)
-    });   
-          
-}, []);
- 
+  if (currentUser) {
+    islogin = true;
+    if (currentUser.role === 'admin') isAdmin = true;
+  }
 
-
-const addComment:any =  (event:any)  => {  
-  
-   console.log('event.target.value ',event.target.value)
-    if(event.target.value!=null){
-      const raw ={"messagetxt": `${event.target.value}`}
-        console.log('raw ', raw)
-      const msgLink = `${api.uri}/articles/${props.id}/msg`
-      return(
-          axios.post(msgLink, raw, {
-            headers: {
-              'Authorization': `Basic ${localStorage.getItem('aToken')}`
-            }
-          })                   
-      .then(reponse =>{ if(reponse.data.message==="added")
-      {
-       alert("comment added")
-       navigate("/");
-       window.location.reload();}
-         else(alert("you have post comment already"))
+  useEffect(() => {
+    const msgLink = `${api.uri}/articles/${props.id}/msg`;
+    axios
+      .get(msgLink)
+      .then((res) => {
+        setComments(res.data);
+        console.log('no of msg ', article_comments.length);
       })
-      .catch(err => {
-      console.log(`${props.msgLink} Check network problems pls. ${props.id}`);
-         alert("Check network problems");
-      })
-      )}
-                 
-}
+      .catch((err) => {
+        console.log('icon error for msg');
+      });
+  }, []);
 
-
-const  removeComm = (msgtxt:any) =>
-{
-Icon=DeleteFilled;
-  const raw = JSON.stringify({"messagetxt": `${msgtxt}`})
-  console.log(' msgLink  ',props.msgLink)   
-  console.log(' raw  ',raw)   
-
-  if(raw!=undefined){
-    const msgLink = `${api.uri}/articles/${props.id}/msg`
-   axios.delete(msgLink, {       
-    headers: {
-        "Authorization": `Basic ${localStorage.getItem('aToken')}`
-      },
-      data: {source:raw}
-      
-    }).then(response =>{ 
-        console.log('msglink ',props.msgLink)
-        console.log('respone ',JSON.stringify(response.data.message))
-        if(response.data.message==="removed")
-      {     
-          alert("This article commentis removed by admin")
-          navigate("/");
-          window.location.reload();}       
-      })
-      .catch(err => {
-      console.log(`${props.type} Check network problems pls. ${props.id}`);
-         alert("Check network problems");
- })
+  const addComment = (value: string) => {
+    if (value !== '') {
+      const raw = { messagetxt: value };
+      const msgLink = `${api.uri}/articles/${props.id}/msg`;
+      axios
+        .post(msgLink, raw, {
+          headers: {
+            Authorization: `Basic ${localStorage.getItem('aToken')}`,
+          },
+        })
+        .then((response) => {
+          if (response.data.message === 'added') {
+            alert('Comment added');
+            navigate('/');
+            window.location.reload();
+          } else {
+            alert('You have already posted a comment');
+          }
+        })
+        .catch((err) => {
+          console.log(`${props.msgLink} Check network problems please. ${props.id}`);
+          alert('Check network problems');
+        });
     }
+  };
 
-  
-  }  
+  const removeComm = (msgtxt: any) => {
+    Icon = DeleteFilled;
+    const raw = JSON.stringify({ messagetxt: `${msgtxt}` });
+    console.log(' msgLink  ', props.msgLink);
+    console.log(' raw  ', raw);
 
- return(
- <>
-  <Button icon={<MessageOutlined />} onClick={()=>{setIsShow(true)}} />
-      <Modal open={isShow} onCancel={()=>{setIsShow(false)}} title="Comments Page " footer={[]}> 
-  
-  <List   
-    className="comment-list"
-    itemLayout="horizontal"
-    dataSource={article_comments}
-    renderItem={(item:any) => (
-    <Flex gap="middle" align='center'  justify = 'flex-start' >
-     <li> 
-        <Comment 
-        //  actions={actions}
-          author={item.username}
-         // avatar={item.avatar}
-        content={item.messagetxt}   
-        datetime={item.datemodified} />                               
-      </li>
-       {isAdmin&&<Icon onClick={() =>removeComm(item.messagetxt)}  />}          
-    </Flex> 
-     )}
-  /> 
-  <Input  placeholder="Pls. login to enter your comments here" name='input_msg' disabled={islogin? false:true}  allowClear
-            onPressEnter={addComment} />
-   </Modal>
-  </> 
-)
-}
+    if (raw !== undefined) {
+      const msgLink = `${api.uri}/articles/${props.id}/msg`;
+      axios
+        .delete(msgLink, {
+          headers: {
+            Authorization: `Basic ${localStorage.getItem('aToken')}`,
+          },
+          data: { source: raw },
+        })
+        .then((response) => {
+          console.log('msglink ', props.msgLink);
+          console.log('response ', JSON.stringify(response.data.message));
+          if (response.data.message === 'removed') {
+            alert('This article comment is removed by admin');
+            navigate('/');
+            window.location.reload();
+          }
+        })
+        .catch((err) => {
+          console.log(`${props.type} Check network problems please. ${props.id}`);
+          alert('Check network problems');
+        });
+    }
+  };
 
+  return (
+    <>
+      <Modal open={isShow} onCancel={() => setIsShow(false)} title="Comments Page " footer={[]}>
+        <List
+          className="comment-list"
+          itemLayout="horizontal"
+          dataSource={article_comments}
+          renderItem={(item: any) => (
+            <Flex gap="middle" align="center" justify="flex-start">
+              <li>
+                <Comment author={item.username} content={item.messagetxt} datetime={item.datemodified} />
+              </li>
+              {isAdmin && <Icon onClick={() => removeComm(item.messagetxt)} />}
+            </Flex>
+          )}
+        />
+        <Row gutter={8} align="middle">
+          <Col flex="auto">
+            <Input
+              placeholder="Please login to enter your comments here"
+              name="input_msg"
+              disabled={!islogin}
+              allowClear
+              onPressEnter={(event) => addComment(event.target.value)}
+            />
+          </Col>
+          <Col>
+            <Button type="primary" onClick={() => addComment(document.getElementsByName('input_msg')[0].value)}>
+              Add Comment
+            </Button>
+          </Col>
+        </Row>
+      </Modal>
+      <Button icon={<MessageOutlined />} onClick={() => setIsShow(true)} />
+    </>
+  );
+};
 
-
-export default DisplayComment
+export default DisplayComment;
